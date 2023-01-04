f0516615e9c8ca344c5c51f8b123eab6640eebff
Tom <wq3570is@go.minnstate.edu>
2023-01-04 15:07:45 -0600

    demo grabbing between view/controller

--- src/main/java/com/springBoot/WebGames/controller/GamesMenuController.java
+++ src/main/java/com/springBoot/WebGames/controller/GamesMenuController.java
@@ -2,13 +2,15 @@ package com.springBoot.WebGames.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/menu")
 public class GamesMenuController {
 
 	@RequestMapping("/tictactoe")
-	public String tictactoe() {
+	public String tictactoe(@RequestParam("press") String pressed) {
+		System.out.println(pressed);
 		return "tictactoe";
 	}
 	
---
+++ src/main/webapp/WEB-INF/jsp/tictactoe.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE>
+<html>
+<head>
+</head>
+
+
+<body>
+
+<form action="tictactoe">
+	<button type="submit" value="1" name="press">name</button>
+
+
+</form>
+
+</body>
+</html>
