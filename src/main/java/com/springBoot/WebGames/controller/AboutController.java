2a504c590d58970c0611237492a2857e3f0c0504
Tom M-pc <105756196+TomMcDonaldMetro@users.noreply.github.com>
2023-02-04 09:50:02 -0600

    add documentation

--- src/main/java/com/springBoot/WebGames/controller/AboutController.java
+++ src/main/java/com/springBoot/WebGames/controller/AboutController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AboutController {
 
+	/**
+	 * 
+	 * @return the jsp name of the about page to be displayed on request.
+	 */
 	@RequestMapping("/about")
 	public String showAbout() {
 		return "about";
--- src/main/java/com/springBoot/WebGames/security/config/SecurityConfig.java
+++ src/main/java/com/springBoot/WebGames/security/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.springBoot.WebGames.security.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
